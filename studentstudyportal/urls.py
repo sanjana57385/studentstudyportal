0d221f415e96bcd196edaaa6b418d45c28ff2bf4
Sanjana Singh <sanjanasingh7774@gmail.com>
2022-04-11 12:38:01 +0530

    Done

--- studentstudyportal/urls.py
+++ studentstudyportal/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
     path('',include('dashboard.urls')),
     path('register/',dash_views.register,name='register'),
     path('login/',auth_views.LoginView.as_view(template_name="dashboard/login.html"),name='login'),
+    path('logout/',auth_views.LoginView.as_view(template_name="dashboard/logout.html"),name='logout'),
     path('profile/',dash_views.profile,name='profile'),
+
 	
 ]
